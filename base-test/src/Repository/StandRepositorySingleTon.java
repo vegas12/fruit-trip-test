c7a2ec471217f2f8b4c56dbadea265b8b48fe621
vegas12 <burger_king89@hotmail.com>
2022-11-19 21:20:32 +0100

    Rewrite part 1.
    Signed-off-by: vegas12 <burger_king89@hotmail.com>

---
+++ base-test/src/Repository/StandRepositorySingleTon.java
@@ -0,0 +1,75 @@
+package com.vegas.fruit_ride.Repository;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.*;
+import static java.util.Map.entry;
+import com.vegas.fruit_ride.Entity.Stand;
+import com.vegas.fruit_ride.Entity.Fruit;
+import java.util.stream.Collectors;
+
+public class StandRepositorySingleTon {
+    private static StandRepositorySingleTon single_instance = null;
+
+    // Since I am not using a file or some SQLite I have no way of persisting data
+    // so this class is singleton and gets mutated during runtime
+    private List<Map<String, Integer>> data;
+
+    public StandRepositorySingleTon() {
+        this.data = Arrays.asList(
+            Map.ofEntries(
+                // Fruit name, price
+                entry("CHERRY", 3),
+                entry("PEACH", 4)
+            ),
+            Map.ofEntries(
+                entry("CHERRY", 3),
+                entry("PEACH", 9)
+            ),
+            Map.ofEntries(
+                entry("PEACH", 7),
+                entry("CHERRY", 3)
+            ),
+            Map.ofEntries(
+                entry("CHERRY", 4),
+                entry("PEACH", 7)
+            )
+        );
+    }
+
+    public static StandRepositorySingleTon getInstance()
+    {
+        if (single_instance == null)
+            single_instance = new StandRepositorySingleTon();
+
+        return single_instance;
+    }
+
+    public List<Stand> getStands() {
+        List<Stand> stands = new ArrayList<>();
+
+        for (Map<String, Integer> stand : data) {
+            List<Fruit> fruits =
+                    stand.entrySet()
+                            .stream()
+                            .map(e -> new Fruit(Fruit.Type.valueOf(e.getKey()), e.getValue()))
+                            .collect(Collectors.toCollection(ArrayList::new));
+            stands.add(
+                    new Stand(fruits)
+            );
+        }
+
+        return stands;
+    }
+
+    public void updateProductsForStandAtIndex(int index, List<Fruit> fruits) throws RuntimeException {
+        if (index > data.size()) {
+            throw new RuntimeException("No Stand at index " + index);
+        }
+
+        this.data.set(index, fruits.stream().collect(Collectors.toMap(p -> p.getType().name(), p -> p.getPrice())));
+    }
+}
---
+++ base-test/src/UseCase/CheapestStandFinder.java
@@ -0,0 +1,92 @@
+package com.vegas.fruit_ride.UseCase;
+
+import com.vegas.fruit_ride.Repository.*;
+import com.vegas.fruit_ride.Entity.*;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.Comparator;
+import java.util.Collections;
+import java.util.function.Predicate;
+
+public class CheapestStandFinder {
+    private StandRepositorySingleTon repository;
+
+    public CheapestStandFinder() {
+        repository = StandRepositorySingleTon.getInstance();
+    }
+
+    public void run(int amountOfFruitsToBuy) {
+        List<Stand> stands = repository.getStands();
+
+        int min = stands.get(0).getFruits()
+            .stream()
+            .mapToInt(Fruit::getPrice)
+            .sum();
+        int prevmin = min;
+        int currentStand = 0;
+        int cheapestAtIndex = 0;
+        List<Fruit> boughtFruits = stands
+                .get(0)
+                .getFruits()
+                .stream()
+                .sorted(Comparator.comparingInt(fruit -> fruit.getPrice()))
+                .limit(amountOfFruitsToBuy)
+                .collect(Collectors.toList());
+
+        for (Stand stand : stands) {
+            List<Fruit> cheapestFruits = stand
+                .getFruits()
+                .stream()
+                .sorted(Comparator.comparingInt(fruit -> fruit.getPrice()))
+                .limit(amountOfFruitsToBuy)
+                .collect(Collectors.toList());
+
+            int sumOfFruits = cheapestFruits
+                .stream()
+                .mapToInt(Fruit::getPrice)
+                .sum();
+
+            min = Math.min(sumOfFruits, min);
+
+            if (min < prevmin) {
+                cheapestAtIndex = currentStand;
+                boughtFruits.clear();
+                boughtFruits.addAll(cheapestFruits);
+                prevmin = min;
+            }
+
+            prevmin = min;
+            currentStand++;
+        }
+
+        System.out.println("Stand: " + ordinal(cheapestAtIndex + 1));
+        System.out.println("Total price: " + min);
+
+        for (Fruit boughtFruit : boughtFruits) {
+            System.out.println("Bought: " + boughtFruit.getType() + " for " + boughtFruit.getPrice());
+        }
+
+        List<Fruit> fruits = stands.get(cheapestAtIndex).getFruits();
+        fruits.removeIf((Fruit fruit) -> boughtFruits.contains(fruit));
+
+        repository.updateProductsForStandAtIndex(
+            cheapestAtIndex,
+            fruits
+        );
+    }
+
+    private static String ordinal(int i) {
+        String[] suffixes = new String[] { "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th" };
+        switch (i % 100) {
+            case 11:
+            case 12:
+            case 13:
+                return i + "th";
+            default:
+                return i + suffixes[i % 10];
+
+        }
+    }
+}
