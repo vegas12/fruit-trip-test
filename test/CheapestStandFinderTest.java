e62cfd0a04dc4c103db1895ac867eec8b3dd975e
vegas12 <burger_king89@hotmail.com>
2022-11-20 18:29:46 +0100

    Add unit tests.
    Signed-off-by: vegas12 <burger_king89@hotmail.com>

--- test/CheapestStandFinderTest.java
+++ test/CheapestStandFinderTest.java
@@ -2,10 +2,10 @@ import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.*;
 import com.vegas.fruit_ride.UseCase.CheapestStandFinder;
+import com.vegas.fruit_ride.Entity.*;
 import java.io.PrintStream;
 import java.io.ByteArrayOutputStream;
 import java.util.HashSet;
-import com.vegas.fruit_ride.Entity.*;
 import java.util.Arrays;
 
 public class CheapestStandFinderTest {
@@ -29,4 +29,18 @@ public class CheapestStandFinderTest {
             "Bought: PEAR for 1\n" +
             "Bought: CHERRY for 3", outputStreamCaptor.toString().trim());
     }
+
+    @Test(expected = RuntimeException.class)
+    public void testExceptionIsThrownWhenThereIsMoreRequiredTypesThanFruitsToBuy() {
+        CheapestStandFinder finder = new CheapestStandFinder();
+        finder.run(1, new HashSet<>(Arrays.asList(Fruit.Type.PEAR, Fruit.Type.PEACH)));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testExceptionIsThrownWhenThereIsMoreRequiredTypesThanFruitsToBuy() {
+        CheapestStandFinder finder = new CheapestStandFinder();
+        finder.run(1, new HashSet<>(Arrays.asList(Fruit.Type.PEAR, Fruit.Type.PEACH)));
+    }
+
+
 }
